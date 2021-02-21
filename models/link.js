0111bfacef36313c18cc83d28ff76741eae33af0
Ywoosang <opellong13@gmail.com>
2021-02-22 08:08:15 +0900

    Add Model table

---
+++ models/link.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize,DataTypes){
+    let link = sequelize.define("Link",{
+        name : {
+            type : DataTypes.STRING(100),
+            allowNull : false
+        },
+        content :{
+            type : DataTypes.TEXT,
+            allowNull: false
+        }
+    });
+    // 
+    link.associate = function(models) {
+        link.belongsTo(models.User,{ foreignKey : {name: 'userId',allowNull:false},onDelete:'CASCADE'});
+    }
+    return link; 
+
+}
