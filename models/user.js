2b322c94dac6e9489208f20c1d844e8cbcfa2c6c
Ywoosang <opellong13@gmail.com>
2021-03-15 19:57:42 +0900

    Don't use unique option in name column

--- models/user.js
+++ models/user.js
@@ -3,28 +3,30 @@ module.exports = function(sequelize,DataTypes){
         name : {
             type : DataTypes.STRING(20),
             allowNull : false,
-            unique : true
         },
-        nickname : {
-            type : DataTypes.STRING(20),
+        email : {
+            type : DataTypes.STRING,
             allowNull :false
         },
         password : {
-            type : DataTypes.STRING(20),
-            allowNull : false
+            type : DataTypes.STRING,
         },
         authId : {
             type : DataTypes.INTEGER(11),
+        },
+        provider : {
+            type : DataTypes.STRING(10),
         }
+        
     });
     // models 는 db 오브젝트
     user.associate = function(models){
         // Link 는 설정한 모델 이름
         user.hasMany(models.Link); 
-    };
-    user.associate = function(models){
         user.hasOne(models.Profile); 
-    }; 
+        user.hasMany(models.Comment);
+        user.hasMany(models.Post);
+    };
     // 
     return user;
 } 
